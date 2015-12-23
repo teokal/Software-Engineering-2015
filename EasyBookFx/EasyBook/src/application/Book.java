95fcd795b8fd13eb7b210285fa9f7bfb9e78aac8
Thodoris Kalatzis <teo.kal@hotmail.com>
2015-12-23 04:18:47 +0200

    Update V1.1.08

---
+++ EasyBookFx/EasyBook/src/application/Book.java
@@ -0,0 +1,163 @@
+package application;
+
+import java.text.ParseException;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Book {
+	private int b_id;
+	private SimpleStringProperty  code;
+	private SimpleStringProperty check_in;
+	private SimpleStringProperty check_out;
+	private SimpleStringProperty  name;
+	private SimpleStringProperty  sname;
+	private SimpleStringProperty  tel;
+	private SimpleStringProperty  email;
+	private SimpleStringProperty  idnum;
+	private SimpleStringProperty  payment_method;
+	private float total_cost;
+	private SimpleStringProperty  paid;
+	private float money_received;
+	private SimpleStringProperty  status;
+	
+	private SimpleStringProperty  room;
+
+	public Book( 
+			int b_id, 
+			String  code, 
+			String  check_in, 
+			String  check_out, 
+			String  name, 
+			String  sname, 
+			String  tel, 
+			String  email, 
+			String  idnum, 
+			String  payment_method, 
+			float total_cost, 
+			String  paid, 
+			float money_received, 
+			String  status) throws ParseException {
+		this.b_id = b_id;
+		this.code = new SimpleStringProperty(code);
+		this.check_in = new SimpleStringProperty(check_in); 
+		this.check_out = new SimpleStringProperty(check_out); 
+		this.name = new SimpleStringProperty(name); 
+		this.sname = new SimpleStringProperty(sname);
+		this.tel = new SimpleStringProperty(tel); 
+		this.email = new SimpleStringProperty(email); 
+		this.idnum = new SimpleStringProperty(idnum); 
+		this.payment_method = new SimpleStringProperty(payment_method); 
+		this.total_cost = total_cost; 
+		this.paid = new SimpleStringProperty(paid); 
+		this.money_received = money_received; 
+		this.status = new SimpleStringProperty(status);
+	}
+
+	private void setB_id (int s) {
+		b_id = s;
+	}
+
+	public void setCode (String  s) {
+		code.set(s);
+	}
+
+	public void setCheck_in (String s) {
+		check_in.set(s);
+	}
+
+	public void setCheck_out (String s) {
+		check_out.set(s);
+	}
+
+	public void setname (String s) {
+		name.set(s);
+	}
+
+	public void setSname (String s) {
+		sname.set(s);
+	}
+
+	public void setTel (String s) {
+		tel.set(s);
+	}
+
+	public void setEmail (String s) {
+		email.set(s);
+	}
+
+	public void setIdnum (String s) {
+		idnum.set(s);
+	}
+
+	public void setPayment_method (String s) {
+		payment_method.set(s);
+	}
+
+	public void setTotal_cost (float s) {
+		total_cost = s;
+	}
+
+	public void setPaid (String s) {
+		paid.set(s);
+	}
+
+	public void setMoney_received (float s) {
+		money_received = s;
+	}
+
+	public void setStatus (String s) {
+		status.set(s);
+	}
+
+	public String getCode () {
+		return code.get();
+	}
+
+	public String getCheck_in () {
+		return check_in.get();
+	}
+
+	public String getCheck_out () {
+		return check_out.get();
+	}
+
+	public String getName () {
+		return name.get();
+	}
+
+	public String getSname () {
+		return sname.get();
+	}
+
+	public String getTel () {
+		return tel.get();
+	}
+
+	public String getEmail () {
+		return email.get();
+	}
+
+	public String getIdnum () {
+		return idnum.get();
+	}
+
+	public String getPayment_method () {
+		return payment_method.get();
+	}
+
+	public float getTotal_cost () {
+		return total_cost;
+	}
+
+	public String getPaid () {
+		return paid.get();
+	}
+
+	public float getMoney_received () {
+		return money_received;
+	}
+
+	public String getStatus () {
+		return status.get();
+	}
+
+}
--- EasyBookFx/EasyBook/src/events/AdministrationPanel.java
+++ EasyBookFx/EasyBook/src/events/AdministrationPanel.java
@@ -2,91 +2,245 @@ package events;
 
 import java.net.URL;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.mysql.jdbc.Statement;
 
+import application.Book;
 import application.Main;
 import database.Conn;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.TableView;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class AdministrationPanel implements Initializable {
 
 	public Connection conn = Conn.connect();
 
 	@FXML
-	private Button newBookBtn;
+	private TableColumn<Book, String> check_in_clmn;
+	@FXML
+	private TableColumn<Book, String> paid_clmn;
+	@FXML
+	private TableColumn<Book, String> name_clmn;
+	@FXML
+	private TableColumn<Book, String> surname_clmn;
+	@FXML
+	private TableColumn<Book, String> check_out_clmn;
+	@FXML
+	private TableColumn<Book, String> tel_clmn;
+	@FXML
+	private TableColumn<Book, String> email_clmn;
+	@FXML
+	private TableColumn<Book, String> idnum_clmn;
+	@FXML
+	private ToggleGroup categoryTypesRooms;
 	@FXML
 	private Button searchBook;
 	@FXML
+	private TableColumn<Book, String> persons_clmn;
+	@FXML
+	private ToggleGroup categoryOffers;
+	@FXML
+	private ToggleGroup categoryRadioDatesOffers;
+	@FXML
+	private ToggleGroup categoryBookings;
+	@FXML
+	private TableColumn<Book, String> code_clmn;
+	@FXML
+	private TableColumn<Book, String> room_clmn;
+	@FXML
+	private ToggleGroup categoryBedRooms;
+	@FXML
+	private Pane bookings_tab;
+	@FXML
 	private TextField searchBookText;
+	@FXML
+	private ToggleGroup categoryRadioTypeOffers;
+	@FXML
+	private ToggleGroup categoryIncomeBooksStatistics;
+	@FXML
+	private TableView<Book> bookingsTable;
+	@FXML
+	private Button newBookBtn;
+	@FXML
+	private ToggleGroup categoryTypeOptions;
+    @FXML
+    private ToggleButton allBookings;
+    @FXML
+    private ToggleButton completedBookings;
+    @FXML
+    private ToggleButton comingSoonBookings;
+    @FXML
+    private ToggleButton runningBookings;
+
+	private ObservableList<Book> bookingList;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
+		showAllBookings(null);
 	}
 
 	public void newBook(ActionEvent event) {
 		Main main = new Main();
 		main.startNewBookPanel();
 	}
+	
+	public void showAllBookings(ActionEvent event) {
+		showBookingsOnTable("Select * from bookings");
+	}
+	
+	public void showRunning(ActionEvent event) {
+		showBookingsOnTable("Select * from bookings"
+				+ " where ( `check_in` >= concat(curdate(), ' ', curtime()) ) "
+				+ "AND (`check_out` < concat(curdate(), ' ', curtime()) )");
+	}
+	
+	public void showComingSoon(ActionEvent event) {
+		showBookingsOnTable("Select * from bookings"
+				+ " where `check_in` < concat( curdate(), ' ', curtime() )");
+	}
+	
+	public void showCompleted(ActionEvent event) {
+		showBookingsOnTable("Select * from bookings"
+				+ " where `check_out` <= concat( curdate(), ' ', curtime() )");
+	}
 
 	public void searchBookFromText(ActionEvent event) {
 
 		String textSearch = searchBookText.getText();
 
-		try {
-			Statement stmt = (Statement) conn.createStatement();
-
-			String query = "SELECT * FROM bookings "
-					+ "WHERE `b_id` LIKE '%" + textSearch + "%'	"
-					+ "OR	`code`  LIKE '%" + textSearch + "%'	"
-					+ "OR	`name` LIKE '%" + textSearch + "%'	"
-					+ "OR	`sname` LIKE '%" + textSearch + "%'	"
-					+ "OR 	`tel` LIKE '%" + textSearch + "%'	"
-					+ "OR	`email` LIKE '%" + textSearch + "%'	";
+		if (! textSearch.isEmpty() ) {
 			
-			ResultSet rs = stmt.executeQuery( query );
+			deselectToggle();
+			
+			try {
+				Statement stmt = (Statement) conn.createStatement();
 
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int columnsNumber = rsmd.getColumnCount();
-			while (rs.next()) {
-				for (int i = 1; i <= columnsNumber; i++) {
-					if (i > 1) System.out.print(",  ");
-					String columnValue = rs.getString(i);
-					System.out.print(columnValue + " " + rsmd.getColumnName(i));
-				}
-				System.out.println("");
+				String query = "SELECT * FROM bookings "
+						+ "WHERE `b_id` LIKE '%" + textSearch + "%'	"
+						+ "OR	`code`  LIKE '%" + textSearch + "%'	"
+						+ "OR	`name` LIKE '%" + textSearch + "%'	"
+						+ "OR	`sname` LIKE '%" + textSearch + "%'	"
+						+ "OR 	`tel` LIKE '%" + textSearch + "%'	"
+						+ "OR	`email` LIKE '%" + textSearch + "%'	";
+
+				showBookingsOnTable(query);
+
+			} catch (SQLException e ) {
+				e.printStackTrace();
 			}
-		} catch (SQLException e ) {
-
 		}
+	}
 
+	public void showBookingsOnTable(String query) {
 
-		if (textSearch != null && textSearch != "") {
-			String query = "Select * from bookings"; //like '" + textSearch + "'";
+		code_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("code") );
+		name_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("name") );
+		surname_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("sname") );
+		check_in_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("check_in") );
+		check_out_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("check_out") );
+		tel_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("tel") );
+		email_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("email") );
+		idnum_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("idnum") );
+		room_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("name") );
+		persons_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("paid") );
+		paid_clmn.setCellValueFactory(new PropertyValueFactory<Book, String>("paid") );
 
-			//ResultSet rs = Conn.getData(this, query);
-			//Conn.printResult(rs);
-		}
+		bookingList = getTableData(query);
+		bookingsTable.setItems( bookingList );
 
+		bookingsTable.getSelectionModel().selectedIndexProperty().addListener(new RowSelectChangeListener());
 
 	}
 
+	private class RowSelectChangeListener implements ChangeListener<Number> {
 
+		@Override
+		public void changed(ObservableValue<? extends Number> ov, 
+				Number oldVal, Number newVal) {
 
+			int ix = newVal.intValue();
 
+			if ((ix < 0) || (ix >= bookingList.size())) {
 
+				return; // invalid data
+			}
+
+			Book book = bookingList.get(ix);
+			//actionStatus.setText(book.toString());	
+		}
+	}
+
+	private ObservableList<Book> getTableData(String query) {
+
+		List<Book> list = new ArrayList<Book>();
+		
+		boolean found = false;
+
+		try {
+			Statement stmt = (Statement) conn.createStatement();
+
+			ResultSet rs = stmt.executeQuery( query );
+
+			while (rs.next()) {
+				found = true;
+				list.add( new Book(
+						rs.getInt("b_id"), 
+						rs.getString("code"), 
+						rs.getString("check_in"),
+						rs.getString("check_out"),
+						rs.getString("name"),
+						rs.getString("sname"),
+						rs.getString("tel"),
+						rs.getString("email"),
+						rs.getString("idnum"),
+						rs.getString("payment_method"),
+						rs.getFloat("total_cost"),
+						rs.getString("paid"),
+						rs.getFloat("money_received"),
+						rs.getString("status") ) ); 
+			}
+		} catch (SQLException e )	{ e.printStackTrace();
+		} catch (ParseException e)	{ e.printStackTrace();	}
+		
+//		if ( !found ){
+//			Alert alert = new Alert(AlertType.INFORMATION);
+//			alert.setTitle("Search Results");
+//			alert.setContentText("Nothing found!");
+//			alert.showAndWait();
+//		}
+		
+		ObservableList<Book> data = FXCollections.observableList(list);
+
+		return data;
+	}
+
+	private void deselectToggle(){
+		allBookings.setSelected(false);
+		completedBookings.setSelected(false);
+		runningBookings.setSelected(false);
+		comingSoonBookings.setSelected(false);
+		
+	}
 }

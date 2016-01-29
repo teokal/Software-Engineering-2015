dd955201d75fe300c3f0b1efe14e1d8e161880ca
Rafsonic <raf-ael@hotmail.com>
2016-01-29 18:03:44 +0200

    Update Statistics

--- EasyBookFx/EasyBook/src/application/Statistics.java
+++ EasyBookFx/EasyBook/src/application/Statistics.java
@@ -1,34 +1,59 @@
 package application;
 
-import java.text.DateFormatSymbols;
-import java.util.Arrays;
-import java.util.Locale;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
+import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
+public class Statistics extends Application{
 
-public class Statistics {
-
-	 @FXML
-	    private BarChart<String, Integer> barChart;
-	 @FXML
-	    private CategoryAxis xAxis;
+	@FXML
+	private LineChart<String, Number> lineChart;
 	
-	 private ObservableList<String> monthNames = FXCollections.observableArrayList();
-	 
-	 @FXML
-	    private void initialize() {
-		 String[] months = DateFormatSymbols.getInstance(Locale.ENGLISH).getMonths();
-		 monthNames.addAll(Arrays.asList(months));
-		 
-		 xAxis.setCategories(monthNames);
-	 }
-	 
-	 XYChart.Series series1 = new XYChart.Series();
-
+	@Override public void start(Stage stage) {
+		
+		
+		
+        stage.setTitle("Line Chart Sample");
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Months");       
+        
+        final LineChart<String,Number> lineChart = 
+                new LineChart<String,Number>(xAxis,yAxis);
+                
+        lineChart.setTitle("Bookings of 2016");
+                                
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        
+        series.getData().add(new XYChart.Data<String, Number>("Jan", 23));
+        series.getData().add(new XYChart.Data<String, Number>("Feb", 14));
+        series.getData().add(new XYChart.Data<String, Number>("Mar", 15));
+        series.getData().add(new XYChart.Data<String, Number>("Apr", 24));
+        series.getData().add(new XYChart.Data<String, Number>("May", 34));
+        series.getData().add(new XYChart.Data<String, Number>("Jun", 36));
+        series.getData().add(new XYChart.Data<String, Number>("Jul", 22));
+        series.getData().add(new XYChart.Data<String, Number>("Aug", 45));
+        series.getData().add(new XYChart.Data<String, Number>("Sep", 43));
+        series.getData().add(new XYChart.Data<String, Number>("Oct", 17));
+        series.getData().add(new XYChart.Data<String, Number>("Nov", 29));
+        series.getData().add(new XYChart.Data<String, Number>("Dec", 25));
+       
+        
+       // Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
+       
+       // stage.setScene(scene);
+       // stage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

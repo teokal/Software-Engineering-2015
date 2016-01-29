b529dac11fbbce24002297bfa7e2969122600e2b
Rafsonic <raf-ael@hotmail.com>
2016-01-30 00:03:53 +0200

    Code statistics

--- EasyBookFx/EasyBook/src/application/Statistics.java
+++ EasyBookFx/EasyBook/src/application/Statistics.java
@@ -5,8 +5,42 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import database.Conn;
+import javafx.beans.property.SimpleStringProperty;
 
 public class Statistics extends Application{
+	
+	try {
+		
+		Connection conn = Conn.connect();
+		String query = "SELECT MONTH(`check_in`) MONTH, COUNT(*) COUNT FROM `bookings` WHERE YEAR(`check_in`)=? GROUP BY MONTH(`check_in`)";
+		int year = 2016;
+		int yearNow, count;
+		PreparedStatement ps = conn.prepareStatement(query);
+
+		ps.setInt(1, year);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while ( rs.next() ) {
+			yearNow = rs.getInt("MONTH");
+			count = rs.getInt("COUNT");
+		}
+		conn.close();
+
+	} catch (IOException|SQLException e) {
+		e.printStackTrace();
+	}
+	
+	
+	
 
 	public LineChart<String, Number> show( LineChart<String, Number> linechart) {
 
